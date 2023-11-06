65217fe8195709136613663db783f7f8d6f8fa26
DimaNarepeha <dimanarepeha@gmail.com>
2023-11-06 20:26:54 +0100

    init

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-class TreeNode:
-    def __init__(self):
-        self.child = []
-        self.independent_sets = [0, 1]  # [0] = not included, [1] = included
+from TreeNode import Tree, TreeNode
+from ThreeSets import stableSet
 
 
-def countIndependentSets(root):
-    def dfs(node):
-        for child in node.child:
-            dfs(child)
 
-        for i in range(2):  # 0 for not included, 1 for included
-            total_sets = 1
-            for child in node.child:
-                if i == 0:
-                    total_sets += (node.independent_sets[1] + child.independent_sets[0])
-                else:
-                    total_sets += (node.independent_sets[0] + child.independent_sets[0])
-            node.independent_sets[i] = total_sets
+'''
+     1
+    2 5
+   3 4 
 
-    dfs(root)
-    return root.independent_sets[0] + root.independent_sets[1]
+'''
+my_tree = Tree(1)  # Root node with data 1
+my_tree.root.children.append(TreeNode(2))
+my_tree.root.children.append(TreeNode(5))
+node_two = my_tree.root.children[0]
+
+node_two.children.append(TreeNode(3))
+node_two.children.append(TreeNode(4))
+
+print(my_tree)
+
+print(stableSet(my_tree.root))
 
 
-# Example usage:
-# Construct a tree and call the countIndependentSets function on the root node
-root = TreeNode()
-root.child = [TreeNode(), TreeNode(), TreeNode()]
-# root.child[0].child = [TreeNode()]
-# root.child[1].child = [TreeNode(), TreeNode()]
-# root.child[1].child[1].child = [TreeNode()]
-
-total_sets = countIndependentSets(root)
-print("Total Independent Sets:", total_sets)

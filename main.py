83c5a7332fdb43da3a2eb5baa2b9884ad4531c0b
DimaNarepeha <dimanarepeha@gmail.com>
2023-11-07 00:04:49 +0100

    Add more comments

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ from ThreeSets import stableSet
      1
     2 5
    3 4 
-
+   
+   
+1-5: 1,2,3,4,5
+6: 2,5
+7: 3,4
+8: 3,5
+9: 2,4
+10: 1,3
+11: 1,4
+12: 1,3,4
+13: 3,4,5
+14: empty set
 '''
 my_tree = Tree(1)  # Root node with data 1
 my_tree.root.children.append(TreeNode(2))

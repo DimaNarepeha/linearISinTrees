2ab5b4172f6e12efe045947734564c4801ae68a5
DimaNarepeha <dimanarepeha@gmail.com>
2023-11-08 21:23:00 +0100

    Added simple cache to avoid excesive recursive calls

--- ThreeSets.py
+++ ThreeSets.py
@@ -1,16 +1,29 @@
 def combinationsWithoutCurrent(current):
-  num = 1
-  for child in current:
-    num *= stableSet(child)
-  return num
+    if current.isCachedWithout:
+        print("used cash for " + str(current.data))
+        return current.cachedWithoutValue
+    num = 1
+    for child in current:
+        num *= stableSet(child)
+    current.cachedWithoutValue = num
+    current.isCachedWithout = True
+    print("calculated for " + str(current.data))
+    return num
+
 
 def combinationsWithCurrent(current):
-  num = 1
-  for child in current:
-    num *= combinationsWithoutCurrent(child)
-  return num
+    if current.isCachedWith:
+        print("used cash for " + str(current.data))
+        return current.cachedWithValue
+    num = 1
+    for child in current:
+        num *= combinationsWithoutCurrent(child)
+    current.cachedWithValue = num
+    current.isCachedWith = True
+    print("calculated for " + str(current.data))
+    return num
+
 
 def stableSet(current):
-  return (combinationsWithCurrent(current) +
-          combinationsWithoutCurrent(current))
-
+    return (combinationsWithCurrent(current) +
+            combinationsWithoutCurrent(current))
--- TreeNode.py
+++ TreeNode.py
@@ -1,16 +1,20 @@
 class TreeNode:
     def __init__(self, data):
         self.data = data
+        self.isCachedWith = False
+        self.isCachedWithout = False
+        self.cachedWithValue = 0
+        self.cachedWithoutValue = 0
         self.children = []
 
     def __iter__(self):
         return iter(self.children)
 
+
 class Tree:
     def __init__(self, root_data):
         self.root = TreeNode(root_data)
 
-    
     def __str__(self):
         return self._str_helper(self.root)
 
@@ -22,13 +26,13 @@ class Tree:
             result += "]"
         return result
 
+
 # Create a tree with 5 nodes
 
 if __name__ == "__main__":
-# Adding child nodes
+    # Adding child nodes
     my_tree = Tree(1)  # Root node with data 1
     my_tree.root.children.append(TreeNode(2))
     my_tree.root.children.append(TreeNode(3))
     my_tree.root.children.append(TreeNode(4))
     my_tree.root.children.append(TreeNode(5))
-

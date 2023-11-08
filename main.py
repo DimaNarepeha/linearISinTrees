4b458e1a0fc1d10e075bd373906fa1cedacb12a0
DimaNarepeha <dimanarepeha@gmail.com>
2023-11-08 21:03:21 +0100

    Changed tree structure for testing

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from TreeNode import Tree, TreeNode
 from ThreeSets import stableSet
 
-
-
 '''
      1
     2 5
    3 4 
-   
-   
+
+
 1-5: 1,2,3,4,5
 6: 2,5
 7: 3,4
@@ -20,16 +18,32 @@ from ThreeSets import stableSet
 13: 3,4,5
 14: empty set
 '''
+
+#
+#     1
+#  2     3
+# 4 5   6 7
 my_tree = Tree(1)  # Root node with data 1
 my_tree.root.children.append(TreeNode(2))
-my_tree.root.children.append(TreeNode(5))
-node_two = my_tree.root.children[0]
+my_tree.root.children.append(TreeNode(3))
 
-node_two.children.append(TreeNode(3))
-node_two.children.append(TreeNode(4))
+child2 = my_tree.root.children[0]
+child3 = my_tree.root.children[1]
+
+child2.children.append(TreeNode(4))
+child2.children.append(TreeNode(5))
+
+child3.children.append(TreeNode(6))
+child3.children.append(TreeNode(7))
+
+# my_tree = Tree(1)  # Root node with data 1
+# my_tree.root.children.append(TreeNode(2))
+# my_tree.root.children.append(TreeNode(5))
+# node_two = my_tree.root.children[0]
+#
+# node_two.children.append(TreeNode(3))
+# node_two.children.append(TreeNode(4))
 
 print(my_tree)
 
 print(stableSet(my_tree.root))
-
-
